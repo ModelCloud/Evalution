379f83a8f8c03131fd8d15b04dfd230152894aba
Qubitium <qubitium@modelcloud.ai>
2026-03-22 02:34:50 +0000

    update headers

--- evalution/engines/base.py
+++ evalution/engines/base.py
@@ -1,3 +1,8 @@
+# SPDX-FileCopyrightText: 2026 ModelCloud.ai
+# SPDX-FileCopyrightText: 2026 qubitium@modelcloud.ai
+# SPDX-License-Identifier: Apache-2.0
+# Contact: qubitium@modelcloud.ai, x.com/qubitium
+
 from __future__ import annotations
 
 from collections.abc import Iterable, Iterator
--- evalution/suites/__init__.py
+++ evalution/suites/__init__.py
@@ -1,3 +1,8 @@
+# SPDX-FileCopyrightText: 2026 ModelCloud.ai
+# SPDX-FileCopyrightText: 2026 qubitium@modelcloud.ai
+# SPDX-License-Identifier: Apache-2.0
+# Contact: qubitium@modelcloud.ai, x.com/qubitium
+
 from .arc_easy import ARCEasy, arc_easy
 from .arc_challenge import ARCChallenge, arc_challenge
 from .base import BaseTestSuite, TestSuite
